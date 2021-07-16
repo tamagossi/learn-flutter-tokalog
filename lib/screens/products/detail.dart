4423e6a723fffdd72179105b798acdb665ea1a74
Raka Pratama <mgf.prauliyatama@gmail.com>
2021-07-16 14:38:14 +0700

    feat(product-detail): construct product-detail page

--- lib/screens/products/detail.dart
+++ lib/screens/products/detail.dart
@@ -1,7 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tokalog/providers/product.dart';
-import 'package:tokalog/widgets/platform/platform_scaffold.dart';
+import 'package:tokalog/widgets/platform/platform_progress_indicator.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   static const routeName = 'product-detail';
@@ -18,17 +19,46 @@ class ProductDetailScreen extends StatefulWidget {
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    // final product = Provider.of<ProductProvider>(context, listen: false).getById(widget.id);
+    final product =
+        Provider.of<ProductProvider>(context, listen: false).getById(widget.id);
 
-    /**
-     * with Consumer approach, you can listen to provider only on a spesific widget
-     * 
-    */
-    return Consumer<ProductProvider>(
-      builder: (ctx, productProvider, child) => PlatformScaffold(
-        appBarText: Text(productProvider.getById(widget.id).title),
-        content: Center(
-          child: Text('Hallo from product detail page'),
+    return Scaffold(
+      appBar: AppBar(title: Text(product.title)),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 300,
+                child: CachedNetworkImage(
+                  imageUrl: product.image,
+                  errorWidget: (_, __, error) => Icon(Icons.error),
+                  fit: BoxFit.cover,
+                  progressIndicatorBuilder: (context, url, progress) =>
+                      PlatformProgressIndicator(progress: progress.progress),
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '\$${product.price}',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                child: Text(
+                  product.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
