5d47a5793bdff4542eea1aa12f34afa6c76fcd6a
Raka Pratama <mgf.prauliyatama@gmail.com>
2021-09-04 10:05:43 +0700

    feat(auth): auth http request functions
    * add function wrapper for signup and login to prevent redundant code

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tokalog/models/exceptions/authentication.dart';
 import 'package:tokalog/services/auth.dart';
 
 class AuthProvider with ChangeNotifier {
@@ -7,21 +8,50 @@ class AuthProvider with ChangeNotifier {
   String _token;
   String _userId;
 
-  final String signupURL = dotenv.env['FIREBASE_SIGNUP_URL'];
-  var _authService = new AuthService();
-  String _errorMessage = 'auth provider error';
+  AuthService _authService = new AuthService();
+
+  final String _errorMessage = 'auth provider error';
+  final String _firebaseAuthUrl = dotenv.env['FIREBASE_AUTH_URL'];
+  final String _APIKey = dotenv.env['FIREBASE_KEY'];
+
+  Future<void> login(Map<String, String> credential) async {
+    this._authenticate(credential, 'LOGIN');
+  }
 
   Future<void> signup(Map<String, String> credential) async {
-    try {
-      _authService = new AuthService();
+    this._authenticate(credential, 'SIGNUP');
+  }
 
-      await _authService.sendCustomPostUrlRequest(signupURL, {
-        'email': credential['email'],
-        'password': credential['password'],
-        'returnSecureToken': true,
-      });
+  Future<void> _authenticate(
+    Map<String, String> credential,
+    String authType,
+  ) async {
+    try {
+      AuthService _authService = new AuthService();
+      const Map<String, String> AUTH_TYPE = {
+        'LOGIN': 'signInWithPassword',
+        'SIGNUP': 'signUp'
+      };
+
+      String url = '$_firebaseAuthUrl${AUTH_TYPE[authType]}?key=$_APIKey';
+
+      final response = await _authService.sendCustomPostUrlRequest(
+        url,
+        {
+          'email': credential['email'],
+          'password': credential['password'],
+          'returnSecureToken': true,
+        },
+      );
+
+      if (response['error'] != 'null') {
+        throw AuthenticationException(
+          message: response['error']['message'],
+          code: response['error']['code'],
+        );
+      }
     } catch (error) {
-      throw ('$_errorMessage - sign up: \n$error');
+      throw ('$_errorMessage - $authType: \n$error');
     }
   }
 }
